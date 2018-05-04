d045a0649d16a3da5fc32d371f9b20597ea53d30
oc <2336990515@qq.com>
2018-05-04 17:12:11 +0800

    add tbcapture

---
+++ application/controller/tbcapture.py
@@ -0,0 +1,22 @@
+from flask import Blueprint
+from funs import jsonRes
+import requests,re
+tbcapture = Blueprint('tbcapture', __name__)
+@tbcapture.route('/<int:goods_id>')
+def index(goods_id):
+	#抓取商品详情页
+	# tb_page = requests.get('https://detail.tmall.com/item.htm?id='+str(goods_id))	#tm
+	tb_page = requests.get('https://item.taobao.com/item.htm?id='+str(goods_id))
+	page_data = tb_page.text
+	res_reg = re.search(r'\"descUrl\":\"([^\"]*)\"', page_data)	#tm
+	if not res_reg:
+		res_reg = re.search(r'[{,]\s*descUrl *:[^\?]*\? *\'([^\']*)\'', page_data)	#tb
+	detail_url = res_reg.group(1) if res_reg else ''	#获取详情数据地址
+	if detail_url:
+		detail_data = requests.get("http:"+detail_url)
+		if detail_data.text:
+			res_detail = [x+'_760x760Q50s50.jpg_.webp' for x in re.findall(r'src=\"([^\"]*)\"', detail_data.text) if re.search(r'.jpg$', x)]
+			# print(res_detail)
+			return jsonRes(res_detail)
+		else: return  jsonRes(404, 'goods detail is not exist')
+	else: return  jsonRes(404, 'goods is not exist')
---
+++ application/lib/funs.py
@@ -0,0 +1,17 @@
+def jsonRes(st='suc', msg='', data={}):
+	import json
+	res = {}
+	#设置返回码
+	status_codes = { 'suc': 200, 'err': 999 }
+	#处理参数
+	if not (type(st) == str or type(st) == int): data, st = (st, 'suc')
+	if type(st) == int:
+		res['st'] = st
+	elif st in status_codes:
+		res['st'] = status_codes[st]
+	else: st, msg = ('suc', st)
+	if not (type(msg) == str or type(msg) == int): data, msg = (msg, '')
+	
+	if msg: res['msg'] = msg
+	if data: res['data'] = data
+	return json.dumps(res)
