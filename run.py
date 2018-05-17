3bc7567dc95fcc2cc62f0d2c6e255044957a2f17
oc <2336990515@qq.com>
2018-05-17 20:13:40 +0800

    modify app variable, modify blueprint register

--- run.py
+++ run.py
@@ -2,12 +2,12 @@
 from flask import Flask,request
 from application.controller.tbcapture import tbcapture as tbcapture_blueprint
 
-app = Flask(__name__, instance_relative_config=True)	#提交前注释掉
-# app = Flask(__name__)		#提交前添加
-app.config.from_object('conf.default')	#默认配置
-app.config.from_object('conf.config')	#私有配置
-app.config.from_pyfile('config.py')	#开发环境配置		#提交前注释掉
+application = Flask(__name__, instance_relative_config=True)	#提交前注释掉
+# application = Flask(__name__)		#提交前添加
+application.config.from_object('conf.default')	#默认配置
+application.config.from_object('conf.config')	#私有配置
+application.config.from_pyfile('config.py')	#开发环境配置		#提交前注释掉
 
+application.register_blueprint(tbcapture_blueprint, url_prefix='/tbcapture')
 if __name__ == '__main__':
-  app.register_blueprint(tbcapture_blueprint, url_prefix='/tbcapture')
-  app.run(debug=app.config['DEBUG'], threaded = True, port=app.config['PORT'])
+  application.run(debug=application.config['DEBUG'], threaded = True, port=application.config['PORT'])
