e9c0d20dc651b25b2e84cd5031de4bc5da635ab9
oc <2336990515@qq.com>
2018-05-23 14:57:13 +0800

    add tbcapture comments

--- application/controller/tbcapture.py
+++ application/controller/tbcapture.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-from flask import Blueprint
+from flask import Blueprint, request
 from funs import jsonRes
-import requests,re
+import requests, re, json
 tbcapture = Blueprint('tbcapture', __name__)
 @tbcapture.route('/<int:goods_id>')
 def index(goods_id):
@@ -18,7 +18,42 @@ def index(goods_id):
 		if detail_data.text:
 			# res_detail = [x+'_760x760Q50s50.jpg_.webp' for x in re.findall(r'src=\"([^\"]*)\"', detail_data.text) if re.search(r'\.(jpg|png)$', x)]
 			res_detail = [x for x in re.findall(r'src=\"([^\"]*)\"', detail_data.text) if re.search(r'\.(jpg|png)$', x)]
-			# print(res_detail)
 			return jsonRes(res_detail)
-		else: return  jsonRes(404, 'goods detail is not exist')
-	else: return  jsonRes(404, 'goods is not exist')
+		else: return jsonRes(404, 'goods detail is not exist')
+	else: return jsonRes(404, 'goods is not exist')
+
+@tbcapture.route('/commentTotal/<int:goods_id>')
+def commentTotal(goods_id):
+	get_data = requests.get('https://rate.taobao.com/detailCommon.htm?auctionNumId='+str(goods_id))
+	try:
+		comment_data = json.loads(get_data.text[3:-1])
+	except:
+		comment_data = ''
+	if comment_data:
+		return jsonRes(comment_data.get('data',''))
+	else:
+		return jsonRes(404, 'goods comment_data is not exist')
+
+@tbcapture.route('/comments/<int:goods_id>')
+def comments(goods_id):
+	req_data = request.args
+	t_page = req_data.get('page', '1')
+	t_page_size = req_data.get('page_size', '20')
+	t_rateType = req_data.get('rateType', '')
+	t_attribute = req_data.get('attribute', '')
+	req_str = str(goods_id)+'&currentPageNum='+t_page+'&pageSize='+t_page_size
+	if t_rateType:
+		req_str += '&rateType='+t_rateType
+	if t_attribute:
+		req_str += '&attribute='+t_attribute
+	get_data = requests.get('https://rate.taobao.com/feedRateList.htm?auctionNumId='+req_str)
+	try:
+		comment_data = json.loads(get_data.text[3:-2])
+	except:
+		comment_data = ''
+	if comment_data:
+		if 'search' in comment_data:
+			comment_data.pop('search')
+		return jsonRes(comment_data)
+	else:
+		return jsonRes(404, 'goods comment_data is not exist')
